c268edad314a5ca98f84264944e785b799aa567f
KokoroWJY <oumainoli@gmail.com>
2019-03-08 16:56:26 +0800

    外星人入侵

--- game_package/alien_invasion.py
+++ game_package/alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 
 def run_game():
@@ -8,12 +10,16 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
-    screen = pygame.display.set_mode((1200, 800))
+        (ai_settings.screen_width, ai_settings.screen_height)
+    )
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(screen)
 
     """ 开始游戏的主循环 """
     while True:
+        gf.check_events()
 
         """ 监视键盘和鼠标时间 """
         for event in pygame.event.get():
@@ -22,6 +28,7 @@ def run_game():
 
         """ 每次循环是都重绘屏幕 """
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         """ 让最近绘制的屏幕可见 """
         pygame.display.flip()
