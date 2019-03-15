e17094e996fb6c341cb9dc492dcd008721257abe
KokoroWJY <oumainoli@gmail.com>
2019-03-15 19:56:18 +0800

    外星人入侵 外星人设置

--- game_package/alien_invasion.py
+++ game_package/alien_invasion.py
@@ -3,7 +3,6 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
-from alien import Alien
 
 
 def run_game():
@@ -18,18 +17,19 @@ def run_game():
     # 创建一艘飞船 一个子弹编组 一个外星人编组
     ship = Ship(ai_settings, screen)
     bullets = Group()
-    alien = Group()
+    aliens = Group()
 
     # 创建外星人群
-    gf.create_fleet(ai_settings, screen, alien)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
 
     """ 开始游戏的主循环 """
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         bullets.update()
-        gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, alien, bullets)
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
